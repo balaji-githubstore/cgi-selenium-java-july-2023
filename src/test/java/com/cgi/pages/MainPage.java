eaa5eb4ff96879c94f333e719a54d11fc8f4e773
Balaji <balaji0017@gmail.com>
2023-07-28 17:54:23 +0530

    Page Object Model Added

---
+++ src/test/java/com/cgi/pages/MainPage.java
@@ -0,0 +1,15 @@
+package com.cgi.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Handle all menu and common elements in the page
+ */
+public class MainPage {
+private WebDriver driver;
+	
+	public MainPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+}
--- src/test/java/com/cgi/test/LoginTest.java
+++ src/test/java/com/cgi/test/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.cgi.base.AutomationWrapper;
+import com.cgi.pages.LoginPage;
 import com.cgi.utilities.DataUtils;
 
 /**
@@ -12,24 +13,29 @@ import com.cgi.utilities.DataUtils;
  */
 public class LoginTest extends AutomationWrapper {
 
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class,groups = {"smoke","login"})
-	public void validLoginTest(String username,String password,String expectedValue) {
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class, groups = { "smoke", "login" })
+	public void validLoginTest(String username, String password, String expectedValue) {
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+		loginPage.clickOnLogin();
 
 		String actualValue = driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
 		Assert.assertEquals(actualValue, expectedValue);
 	}
 
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class,groups = {"login"})
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class, groups = { "login" })
 	public void invalidLoginTest(String username, String password, String expectedError) {
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
-		String actualError = driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
-		Assert.assertEquals(actualError, expectedError);
+		LoginPage loginPage = new LoginPage(driver);
+
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+		loginPage.clickOnLogin();
+
+		Assert.assertEquals(loginPage.getInvalidErrorMessage(), expectedError);
 	}
-	
+
 }
